7da934300d5f44e1ac7167f6efe3c6a74b822635
SUIANE MIRAELE OLIVEIRA DE SOUZA <a95257295@rn.senac.local>
2025-10-14 16:59:14 -0300

    feat: cotinuação

--- Aula07/src/controllers/livroController.js
+++ Aula07/src/controllers/livroController.js
@@ -13,11 +13,11 @@ export default class LivroController {
                 return;
             }
             const livro = livros.findIndex(f => f.titulo.toLowerCase() === titulo.toLowerCase());
-            if(livro !== -1) {
+            if (livro !== -1) {
                 res.status(400).json({ msg: "Título Já existente." });
                 return;
             }
-            if (Number(ano) <= 1000 || Number(ano) > new Date().getFullYear()) {
+            if (Number(ano) <= 1800 || Number(ano) > new Date().getFullYear()) {
                 res.status(400).json({ msg: "Ano inválido." });
                 return;
             }
@@ -35,6 +35,82 @@ export default class LivroController {
 
         }
     }
+    static listarPorId(req, res) {
+        try {
+            const id = parseInt(req.params.id);
+            const livro = livros.find(l => l.id === id);
+            if (!livro) {
+                res.status(400).json({ msg: "Livro não encontrado." });
+                return;
+            }
+            res.status(200).json(livro);
+        } catch (error) {
+            res.status(500).json({ msg: "Erro ao buscar o livro", erro: error.message });
+        }
+    }
+    static atualizar(req, res) {
 
+        try {
+            const id = parseInt(req.params.id);
+            const { titulo, autor, ano } = req.body;
+            const livro = livros.find(l => l.id === id);
+            if (!livro) {
+                res.status(400).json({ msg: "Livro não encontrado." });
+                return;
+            }
+            if (!titulo || !autor || !ano) {
+                res.status(400).json({ msg: "Preenche todos os campos." });
+                return;
+            }
+            if (livros.findIndex(f => f.titulo.toLowerCase() === titulo.toLowerCase() === titulo.toLowerCase()) !== -1) {
+                res.status(400).json({ msg: "Título Já existente." });
+                return;
+
+            }
+            if (Number(ano) <= 1800 || Number(ano) > new Date().getFullYear()) {
+                res.status(400).json({ msg: "Ano inválido." });
+                return;
+            }
+            livro.titulo = titulo;
+            livro.autor = autor;
+            livro.ano = Number(ano);
+            res.status(200).json({ msg: "Livro atualizado com sucesso!", livro: livro });
+
+        }
+        catch (error) {
+            res.status(500).json({ msg: "Erro ao buscar o livro", erro: error.message });
+        }
+
+
+    }
+    static deletar(req, res) {
+        try {
+            const id = parseInt(req.params.id);
+            const index = livros.findIndex(l => l.id === id);
+            if (index === -1) {
+                res.status(400).json({ msg: "Livro não encontrado." });
+                return;
+            }
+            livros.splice(index, 1);
+            res.status(200).json({ msg: "Livro deletado com sucesso!" });
+         }catch (error) {
+            res.status(500).json({ msg: "Erro ao deletar o livro", erro: error.message });
+        }
+    
+}
+static buscarPorAutor(req, res) {
+    try {
+        const autor = req.params.autor.toLowerCase();
+        const livrosFiltrados = livros.filter(l => l.autor.toLowerCase().includes(autor));
+        if (livrosEncotrados.length === 0) {
+            res.status(404).json({ msg: "Nenhum livro encontrado para esse autor." });
+            return;
+        }
+        res.status(200).json(livrosEncotrados);
+
+}catch (error) {
+        res.status(500).json({ msg: "Erro ao buscar o livro por autor", erro: error.message });
+    }
+}
 
 }
--- Aula07/src/routes/livroRoules.js
+++ Aula07/src/routes/livroRoules.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 router.get('/', LivroController.listar);
 router.post('/', LivroController.criar);
+router.get('/:id', LivroController.listarPorId);
+router.put('/:id', LivroController.atualizar);
+router.delete('/:id', LivroController.deletar);
+router.get('/:autor', LivroController.buscarPorAutor);
 
 export default router;
