fb896df729a93a900d80fed3f764de22179f82c4
SUIANE MIRAELE OLIVEIRA DE SOUZA <a95257295@rn.senac.local>
2025-10-29 17:02:02 -0300

    feat: complemento da aula 11

--- Aula11/src/controllers/UsuarioController.js
+++ Aula11/src/controllers/UsuarioController.js
@@ -1,68 +1,123 @@
 import UsuarioModel from "../models/UsuarioModel.js";
 import axios from "axios";
 
-export default class UsuarioController{
+export default class UsuarioController {
     //Listar todos
-    static listar(req, res){
+    static listar(req, res) {
         try {
             const usuarios = UsuarioModel.listar();
-            if(usuarios.length===0){
-                res.status(200).json({msg: "Nenhum usuário cadastrado"});
+            if (usuarios.length === 0) {
+                res.status(200).json({ msg: "Nenhum usuário cadastrado" });
                 return
-        }
-        if(!usuarios){
-            res.status(400).json({msg: "Erro ao buscar ao listar os usuários"});
-            return
-        }
-        res.status(200).json(usuarios);
-    } catch (error) {
-        res.status(500).json({msg: "Erro interno", erro: error.message});
-    }      
-        }
-        //Buscar por id
-        static buscarPorId(req, res) {
-            try {
-                const id = req.params.id
-                const usuario = UsuarioModel.buscarPorId(id);
-                if(!usuario){
-                   res.status(404).json({msg: "Usuario não encontrado!"});
-                   return 
-                }
-                res.status(200).json(usuario);   
             }
-            catch (error) {
-                res.status(500).json({msg: "Erro interno", erro: error.message})
+            if (!usuarios) {
+                res.status(400).json({ msg: "Erro ao buscar ao listar os usuários" });
+                return
             }
+            res.status(200).json(usuarios);
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno", erro: error.message });
         }
-        //criar
-        static async criar(req, res) {
-            try {
-                const {nome, cep, numero, telefone} = req.body;
-                if(!nome || !cep || !numero || !telefone){
-                    res.status(400).json({msg: "todos os campos devem ser preenchidos!"});
-                    return;
-                }
-                const buscaCep = await axios.get(`https://viacep.com.br/ws/${cep}/json`);
-                if(buscaCep.erro){
-                    res.status(400).json({msg: "CEP inválido"});
-                    return;
-                }
-                const novoUsuario = {
-                    id: Date.now(),
-                    nome: nome,
-                    telefone: telefone,
-                    cep: cep,
-                    rua: buscaCep.logradouro,
-                    numero: numero,
-                    bairro: buscaCep.data.bairro,
-                    cidade: buscaCep.data.cidade,
-                    estado: buscaCep.data.estado
-                }
-                const userCriado = UsuarioModel.criar(novoUsuario);
-                res.status(202).json(userCriado);
-            
-            } catch (error){
-                res.status(500).json({msg: "Erro interno", erro: error.message});
+    }
+    //Buscar por id
+    static buscarPorId(req, res) {
+        try {
+            const id = req.params.id
+            const usuario = UsuarioModel.buscarPorId(id);
+            if (!usuario) {
+                res.status(404).json({ msg: "Usuario não encontrado!" });
+                return
             }
+            res.status(200).json(usuario);
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno", erro: error.message });
         }
-}
+    }
+    //criar
+    static async criar(req, res) {
+        try {
+            const { nome, cep, numero, telefone } = req.body;
+            if (!nome || !cep || !numero || !telefone) {
+                res.status(400).json({ msg: "Todos os campos devem ser preenchidos!" });
+                return;
+            }
+            const buscaCep = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+            if (buscaCep.erro) {
+                res.status(400).json({ msg: "CEP inválido!" });
+                return;
+            }
+            // console.log(buscaCep);
+            const novoUsuario = {
+                id: Date.now(),
+                nome: nome,
+                telefone: telefone,
+                cep: cep,
+                rua: buscaCep.data.logradouro,
+                numero: numero,
+                bairro: buscaCep.data.bairro,
+                cidade: buscaCep.data.localidade,
+                estado: buscaCep.data.uf
+            }
+            const userCriado = UsuarioModel.criar(novoUsuario);
+            res.status(201).json(userCriado);
+
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno", erro: error.message });
+        }
+    }
+
+    static async atualizar(req, res) {
+        try {
+            const id = req.params.id;
+            const { nome, cep, numero, telefone } = req.body;
+            if (!nome || !cep || !numero || !telefone) {
+                res.status(400).json({ msg: "Todos os campos devem ser preenchidos!" });
+                return;
+            }
+            const buscaCep = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+            if (buscaCep.erro) {
+                res.status(400).json({ msg: "CEP inválido!" });
+                return;
+            }
+            const dadosAtualizados = {
+                nome: nome,
+                telefone: telefone,
+                cep: cep,
+                rua: buscaCep.data.logradouro,
+                numero: numero,
+                bairro: buscaCep.data.bairro,
+                cidade: buscaCep.data.localidade,
+                estado: buscaCep.data.uf
+            }
+            const userAtualizado = UsuarioModel.atualizar(id, dadosAtualizados);
+            if(!userAtualizado){
+                res.status(404).json({msg: "Usuário não encontrado!"});
+                return;
+            }
+            res.status(201).json({msg: "User atualizado com sucesso!", user: userAtualizado});
+
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno", erro: error.message });
+        }
+    }
+
+    static deletar(req, res){
+        try {
+            const id = req.params.id;
+            const userDelete = UsuarioModel.deletar(id);
+            if(!userDelete){
+                res.status(404).json({msg: "Usuário não encontrado"});
+                return;
+            }
+            res.status(200).json({msg: "Usuário deletado com Sucesso!"});
+        } catch (error) {
+              res.status(500).json({ msg: "Erro interno", erro: error.message });
+        }
+    }
+
+}   
+    
+
+
+
+
--- Aula11/src/models/UsuarioModel.js
+++ Aula11/src/models/UsuarioModel.js
@@ -1,26 +1,61 @@
-import { usuarios } from "../data/banco.js"
-export default class UsuarioModel {
-    static criar(usuario) {
+import { usuarios } from "../data/banco.js";
+export default class UsuarioModel{
+    static listar(){
+        return usuarios;
+    }
+    static buscarPorId(id){
+        return usuarios.find(u=> u.id === parseInt(id));
+    }
+    static criar(usuario){
         usuarios.push(usuario);
         return usuario;
     }
     static atualizar(id, novosDados){
         const index = usuarios.findIndex(u=> u.id === parseInt(id));
-        if(index === -1) {
+        if(index === -1){
             return null;
         }
-    
-    usuarios[index] = {...usuarios[index], ...novosDados};
-    return usuarios[index];
-}
-static deletar(id){
-    const index = usuarios.findIndex(u=> u.id === parseInt(id));
-    if(index === -1){
-        return false;
+        usuarios[index] = {...usuarios[index], ...novosDados};
+        return usuarios[index];
     }
-    usuarios.splice(index, 1);
-    return true;
-}
-}
+    static deletar(id){
+         const index = usuarios.findIndex(u=> u.id === parseInt(id));
+        if(index === -1){
+            return false;
+        }
+        usuarios.splice(index, 1);
+        return true;
+    }
 
+       static buscarPorCidade (cidade) {
+               return usuarios.filter(l => l.cidade.tolowerCase() === cidade.toLorwerCase());
+              
+}
+       static buscarPorEstado (estado) {
+               return usuarios.filter(l => l.estado.toLowerCase() === estado.toLowerCase());
 
+}
+         static buscarPorNome (nome) {
+                   return usuarios.sort((a, b) => a.nome.localeCompare(b.nome));
+
+         }
+         static buscarPorCep (cep) {
+                    return usuarios.filter(l => l.cep === cep);
+        }
+        static buscarPorBairro (bairro) {
+                    return usuarios.filter(l => l.bairro.toLowerCase() === bairro.toLowerCase());
+    }
+    static ordenar() {
+        let cidades = [];
+        let estados = [];
+        usuarios.forEach((usuario) => {
+            if (!cidades.includes(usuario.cidade)) {
+                cidades.push(usuario.cidade);
+            }
+            if (!estados.includes(usuario.estado)) {
+                estados.push(usuario.estado);
+            }
+        });
+        return {user: usuarios.length, cidades: cidades.length, estados: estados.length};
+    }
+}
